f2f894f443254ef1b64a444e93481cc389ac80b4
Cristiano Baptista <cristianobaptista@designdesu.com>
2017-02-24 14:18:25 +0100

    CI: adds MetaboxData unit tests

--- src/Helpers/MetaboxData.php
+++ src/Helpers/MetaboxData.php
@@ -2,82 +2,36 @@
 
 namespace Ponticlaro\Bebop\Cms\Helpers;
 
-use Ponticlaro\Bebop\Common\Collection;
+use Ponticlaro\Bebop\Common\Patterns\CollectionAbstract;
 
-class MetaboxData {
+class MetaboxData extends CollectionAbstract {
 
-    /**
-     * List of environments
-     * 
-     * @var object Ponticlaro\Bebop\Common\Collection;
-     */
-    private $__data;
+  /**
+    * Gets data with target key
+    * 
+    * @param  string  $key       Key to get data from
+    * @param  boolean $is_single False if we assume there is an array of values, true if only a single value
+    * @return mixed              Data contained in target key
+    */
+  public function get($key, $is_single = false) 
+  {
+    // Get data from container
+    $data = $this->__get($key);
 
-    /**
-     * Instantiated a single metadata container
-     * 
-     * @param array $data Array of data
-     */
-    public function __construct(array $data = array()) 
-    {
-        $this->__data = new Collection($data);
+    // If array and single item is requested, try to unserialize it
+    if ($data && is_array($data) && $is_single) {
+        
+      // Get first item
+      $data = isset($data[0]) ? maybe_unserialize($data[0]) : '';
     }
 
-    /**
-     * Replace data container
-     * 
-     * @param Ponticlaro\Bebop\Common\Patterns\CollectionInterface $container Data container
-     */
-    public function setDataContainer(\Ponticlaro\Bebop\Common\Patterns\CollectionInterface $container)
-    {
-        // Get currently stored data
-        $current_data = $this->__data->getAll();
+    // Handle arrays that only contain empty values
+    elseif (is_array($data) && !array_filter($data) && $is_single) {
 
-        // Set container and pass current data
-        $this->__data = new $container($current_data);
+      $data = '';
     }
 
-    /**
-     * Gets data with target key
-     * 
-     * @param  string  $key       Key to get data from
-     * @param  boolean $is_single False if we assume there is an array of values, true if only a single value
-     * @return mixed              Data contained in target key
-     */
-    public function get($key, $is_single = false) 
-    {
-        // Get data from container
-        $data = $this->__data->get($key);
-
-        // If array and single item is requested, try to unserialize it
-        if ($data && is_array($data) && $is_single) {
-            
-            // Get first item
-            $data = isset($data[0]) ? maybe_unserialize($data[0]) : '';
-        }
-
-        // Handle arrays that only contain empty values
-        elseif (is_array($data) && !array_filter($data) && $is_single) {
-
-            $data = $is_single ? '' : array();
-        }
-
-        // Return data
-        return $data;
-    }
-
-    /**
-     * Sends all undefined method calls to the data collection object
-     * 
-     * @param  string $name Method name
-     * @param  array  $args Method arguments
-     * @return mixed        Method returned value
-     */
-    public function __call($name, $args)
-    {
-        if (!method_exists($this->__data, $name))
-            throw new \Exception("MetaboxData->$name method do not exist", 1);
-
-        return call_user_func_array(array($this->__data, $name), $args);
-    }
+    // Return data
+    return $data;
+  }
 }
---
+++ tests/unit/Helpers/MetaboxDataCest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Helpers;
+
+use \UnitTester;
+use AspectMock\Test;
+use Ponticlaro\Bebop\Cms\Helpers\MetaboxData;
+
+class MetaboxDataCest
+{
+  /**
+   * List of mock instances
+   * 
+   * @var array
+   */
+  private $m = [];
+
+  public function _before(UnitTester $I)
+  {
+    // Mocks
+    $this->m['maybe_unserialize'] = Test::func('Ponticlaro\Bebop\Cms\Helpers', 'maybe_unserialize', '/wp-admin/');
+  }
+
+  public function _after(UnitTester $I)
+  {
+    Test::clean();
+  }
+
+  /**
+   * @author cristianobaptista
+   * @covers Ponticlaro\Bebop\Cms\Helpers\MetaboxData::get
+   * 
+   * @param UnitTester $I Tester Module
+   */
+  public function getSingleContainingSingleValue(UnitTester $I)
+  {
+    $data = new MetaboxData([
+      'key' => 'value'
+    ]);
+
+    $I->assertEquals($data->get('key', true), 'value');
+  }
+
+  /**
+   * @author cristianobaptista
+   * @covers Ponticlaro\Bebop\Cms\Helpers\MetaboxData::get
+   * 
+   * @param UnitTester $I Tester Module
+   */
+  public function getSingleContainingArrayValue(UnitTester $I)
+  {
+    $data = new MetaboxData([
+      'key' => [
+        'value_1',
+        'value_2',
+      ]
+    ]);
+
+    $I->assertEquals($data->get('key', true), [
+      'value_1',
+      'value_2',
+    ]);
+
+    // Verify that maybe_unserialize was invoked
+    $this->m['maybe_unserialize']->verifyInvokedOnce([
+      [
+        'value_1',
+        'value_2',
+      ]
+    ]);
+  }
+
+  /**
+   * @author cristianobaptista
+   * @covers Ponticlaro\Bebop\Cms\Helpers\MetaboxData::get
+   * 
+   * @param UnitTester $I Tester Module
+   */
+  public function getEmptyValue(UnitTester $I)
+  {
+    $data = new MetaboxData([
+      'key' => []
+    ]);
+
+    $I->assertEquals($data->get('key', true), '');
+    $I->assertEquals($data->get('key'), []);
+  }
+}

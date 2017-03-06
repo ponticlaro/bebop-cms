3ea3f9cd808c386c0c0869b4fd8f5a52978ef376
Cristiano Baptista <cristianobaptista@designdesu.com>
2017-03-06 18:04:53 +0100

    removes placeholders for not implemented shortcode presets

--- src/Helpers/ShortcodeFactory.php
+++ src/Helpers/ShortcodeFactory.php
@@ -18,20 +18,9 @@ class ShortcodeFactory extends Factory {
    * @var array
    */
   protected static $manufacturable = array(
-    'facebook_post'   => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\FacebookPost',
-    'faq_list'        => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\FaqList',
-    'form'            => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Form',
-    'formstack'       => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Formstack',
-    'gallery'         => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Gallery',
-    'google_calendar' => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\GoogleCalendar',
-    'google_map'      => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\GoogleMap',
-    'image'           => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Image',
-    'pardot_form'     => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\PardotForm',
-    'quote'           => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Quote',
-    'soundcloud'      => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Soundcloud',
-    'tweet'           => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Tweet',
-    'video'           => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Video',
-    'vimeo'           => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Vimeo',
-    'youtube'         => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Youtube',
+    'image'   => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Image',
+    'video'   => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Video',
+    'vimeo'   => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Vimeo',
+    'youtube' => 'Ponticlaro\Bebop\Cms\Presets\Shortcodes\Youtube',
   );
 }
